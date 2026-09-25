79138a44c28266e3c85c788b4d8a3e74ae5bed99
shrutikawadkar2026 <shruti.k.infodales@gmail.com>
2026-09-25 12:10:50 +0530

    added contact page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@
 import CareerPage from "../pages/career/Career";
 import ServicesPage from "../pages/serviceAtInfodales/Service";
 import BlogPage from "../pages/blog/blog";
+import ContactPage from "../pages/contact/contact";
 
 // `showInNav` controls whether it appears in Header/Footer nav automatically.
 const routes = [
@@ -15,7 +16,7 @@ const routes = [
     { path: "/career", label: "Career", element: <CareerPage />, showInNav: true, showInFooter: true },
     { path: "/services", label: "Services", element: <ServicesPage />, showInNav: true, showInFooter: true },
     { path: "/blog", label: "Blog", element: <BlogPage />, showInNav: true, showInFooter: true },
-
+    { path: "/contact", label: "Contact", element: <ContactPage />, showInNav: true, showInFooter: true },
     // { path: "/contact", label: "Contact", element: <ContactPage />, showInNav: true },
 ];
 
