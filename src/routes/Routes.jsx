426a9a8166159c8b78faf5b9aa6b2246b81e97e1
shruti.infodales <shruti.infodales@gmail.com>
2026-09-25 13:40:43 +0530

    about us page router assets and emailjs dependency added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,6 +7,7 @@ import HomePage from "../pages/home/Home";
 import ServicesPage from "../pages/serviceAtInfodales/Service";
 import BlogPage from "../pages/blog/blog";
 import ContactPage from "../pages/contact/contact";
+import AboutUsPag from "../pages/about/AboutUs";
 
 // `showInNav` controls whether it appears in Header/Footer nav automatically.
 const routes = [
@@ -15,6 +16,7 @@ const routes = [
     // { path: "/services", label: "Services", element: <ServicesPage />, showInNav: true },
     // { path: "/blogs", label: "Blogs", element: <BlogsPage />, showInNav: true },
     { path: "/", label: "Home", element: <HomePage />, showInNav: true, showInFooter: true },
+    { path: "/about", label: "About Us", element: <AboutUsPag />, showInNav: true, showInFooter: true },
     { path: "/career", label: "Career", element: <CareerPage />, showInNav: true, showInFooter: true },
     { path: "/services", label: "Services", element: <ServicesPage />, showInNav: true, showInFooter: true },
     { path: "/blog", label: "Blog", element: <BlogPage />, showInNav: true, showInFooter: true },
